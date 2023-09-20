f52fa1a6a5f27e9030e2a5768aa2bf991afa53b6
Hari <harikarumuri1086@gmail.com>
2023-09-20 09:18:57 +0530

    register route

--- authserver/users/serializers.py
+++ authserver/users/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Note
+from django.contrib.auth.models import User
 
 
 class NoteSerializer(ModelSerializer):
     class Meta:
         model = Note
         fields = '__all__'
+
+
+class UserRegistrationSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')  # Add other fields as needed
+        extra_kwargs = {'password': {'write_only': True}}
--- authserver/users/views.py
+++ authserver/users/views.py
@@ -3,6 +3,10 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
+from rest_framework import status
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -41,3 +45,14 @@ def getNotes(request):
     notes = user.note_set.all()
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])  # Allow registration for unauthenticated users
+def register_user(request):
+    if request.method == 'POST':
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Create a new user
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
